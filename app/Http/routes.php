79abc28e3e4a2fc763843380253a3713129be4c0
Marinho Piacentinis <miacentinis@gmail.com>
2015-09-30 22:49:58 -0400

    Commit estrutura do Post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*Route::get('/', function () {
+    return view('home');
 });
+*/
+Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
